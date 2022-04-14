de22636cbe5cb3d2a13f13e5cc0689e058893e6e
henriqueholz <henriqueholz@live.com>
2022-04-14 20:11:47 -0300

    Creating layers

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json("message: hello world");
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
