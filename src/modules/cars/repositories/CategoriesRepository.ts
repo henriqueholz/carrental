90560a4ffaf8e8623a2a84b98fc43c8db6463a7f
henriqueholz <henriqueholz@live.com>
2022-04-15 12:51:33 -0300

    Adding singleton pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,11 +1,23 @@
 import { Category } from "../model/Category";
-import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-class CategoriesRepository {
-  private categories: Category[] = [];
+class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
+
+  private static INSTANCE: CategoriesRepository;
 
   constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
