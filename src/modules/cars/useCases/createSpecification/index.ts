1c1d79e7bfe2e7771a7e9be01cd71725c1e4b97a
henriqueholz <henriqueholz@live.com>
2022-04-15 15:59:42 -0300

    Creating import category funcionality

--- src/modules/cars/useCases/createSpecification/index.ts
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -1,8 +1,8 @@
-import { SpecificationsRepository } from "../../repositories/implementations/SpecificationRepository";
+import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
 import { CreateSpecificationController } from "./CreateSpecificationController";
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
-const specificationRepository = SpecificationRepository.getInstance();
+const specificationRepository = SpecificationsRepository.getInstance();
 const createSpecificationUseCase = new CreateSpecificationUseCase(
   specificationRepository
 );
