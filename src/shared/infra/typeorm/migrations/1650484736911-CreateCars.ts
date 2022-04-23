0dd52a3ecef1f771196ff2f1ffa5e10c0b0ac9d0
henriqueholz <henriqueholz@live.com>
2022-04-22 23:14:35 -0300

    Creating specification cars relation migration?

--- src/shared/infra/typeorm/migrations/1650484736911-CreateCars.ts
+++ src/shared/infra/typeorm/migrations/1650484736911-CreateCars.ts
@@ -1,4 +1,9 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
 
 export class CreateCars1650484736911 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
